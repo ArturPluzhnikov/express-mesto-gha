ba35e860a6760cab1ec0ab661d001f5d1cd35cad
Artur Pluzhnikov <artur@artbox-btl.ru>
2022-08-23 14:53:27 +0300

    14th project, 4th commit

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const { errorHandler } = require('./errors/errorHandler');
 
 const { PORT = 3000 } = process.env;
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 const app = express();
 
 app.use(bodyParser.json());
